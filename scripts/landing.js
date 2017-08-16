3b9a34ede5b8dd87f845d78043c18e176dc979f2
Nich Bailey <nichbailey@gmail.com>
2017-08-15 22:17:10 -0600

    portfolio and about me changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -125,8 +125,43 @@ var waypoint = new Waypoint({
   handler: function(direction) {
     if (direction == "down") {
       $('.navs').css("color", "black")
+      $(".navs").hover(function(){
+        $(this).css("color", "rgb(68,132,206)");
+        }, function(){
+        $(this).css("color", "black")
+      });
       $('#nav-contact').css("color", "rgb(68,132,206)");
     }
   },
   offset: '30%'
 })
+
+var waypoint = new Waypoint({
+  element: $('#contact'),
+  handler: function(direction) {
+      $('.navs').css("color", "black")
+      $(".navs").hover(function(){
+        $(this).css("color", "rgb(68,132,206)");
+        }, function(){
+        $(this).css("color", "black")
+      });
+      $('#nav-contact').css("color", "rgb(68,132,206)");
+  },
+})
+
+$('.bwWrapper').BlackAndWhite({
+        hoverEffect : true, // default true
+        // set the path to BnWWorker.js for a superfast implementation
+        webworkerPath : false,
+        // to invert the hover effect
+        invertHoverEffect: false,
+        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)
+        intensity:1,
+        speed: { //this property could also be just speed: value for both fadeIn and fadeOut
+            fadeIn: 200, // 200ms for fadeIn animations
+            fadeOut: 400 // 800ms for fadeOut animations
+        },
+        onImageReady:function(img) {
+            // this callback gets executed anytime an image is converted
+        }
+    });
